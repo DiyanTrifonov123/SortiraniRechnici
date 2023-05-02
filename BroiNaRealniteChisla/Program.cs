ffe0580d550e37f61597dd89e22015575c86742c
DiyanTrifonov <diyantrifonov5@gmail.com>
2023-05-02 16:28:57 +0300

    First commit

---
+++ BroiNaRealniteChisla/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BroiNaRealniteChisla
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (counts.ContainsKey(nums[i]))
+                {
+                    counts[nums[i]]++;
+                }
+                else
+                {
+                    counts.Add(nums[i], 1);
+                }
+            }
+
+            foreach (var item in counts.Keys)
+            {
+                Console.WriteLine($"{item} -> {counts[item]}");
+            }
+        }
+    }
+}
---
+++ PodobrenTelefonenUkazatel/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodobrenTelefonenUkazatel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, string> phoneBook = new SortedDictionary<string, string>();
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            while (input[0] != "END")
+            {
+                switch (input[0])
+                {
+                    case "A":
+                        phoneBook.Add(input[1], input[2]); break;
+
+                    case "S":
+                        if (phoneBook.ContainsKey(input[1]))
+                        {
+                            string phone = phoneBook[input[1]];
+                            Console.WriteLine($"{input[1]} -> {phone}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Контактът {input[1]} не съществува!");
+                        }
+                        break;
+                    case "ListAll":
+                        Console.WriteLine($"В указателя има {phoneBook.Count} записани. \nСписък по азбучен ред: ");
+                        foreach (var item in phoneBook.Keys)
+                        {
+                            Console.WriteLine($"{item} -> {phoneBook[item]}");
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+                input = Console.ReadLine().Split().ToArray();
+            }
+        }
+    }
+}
